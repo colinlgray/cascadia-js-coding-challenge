60a844ff947da5f69f547b9626944cbd840bf467
Colin Gray <colin@cgray.dev>
2019-10-29 14:00:36 -0700

    Problem 1 solution

--- src/problem-1.js
+++ src/problem-1.js
@@ -1,5 +1,20 @@
 const userBySkill = users => {
-  throw new Error("not implemented");
+  const skillMap = {};
+  if (!users) {
+    return skillMap;
+  }
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < users[i].skills.length; j++) {
+      if (!skillMap[users[i].skills[j]]) {
+        skillMap[users[i].skills[j]] = [];
+      }
+      skillMap[users[i].skills[j]] = skillMap[users[i].skills[j]].concat(
+        users[i].name
+      );
+    }
+  }
+
+  return skillMap;
 };
 
 module.exports = userBySkill;
--- test/all_tests.js
+++ test/all_tests.js
@@ -1,39 +1,38 @@
 const assert = require("assert"),
   solutions = require("../src");
 
-//check a 2D array on ints that contains a sudoku solution to see if it’s a valid solution
 describe("Problem 1", () => {
-  const users = [
-    {
-      id: 0,
-      name: "John",
-      skills: ["javascript", "html", "css", "c#"]
-    },
-    {
-      id: 1,
-      name: "Brian",
-      skills: ["javascript", "java", "c", "c#", "c++", "html"]
-    },
-    {
-      id: 2,
-      name: "Michael",
-      skills: ["c", "c++", "go", "rust"]
-    }
-  ];
-
-  const expected = {
-    c: ["Brian", "Michael"],
-    "c#": ["John", "Brian"],
-    "c++": ["Brian", "Michael"],
-    css: ["John"],
-    go: ["Michael"],
-    html: ["John", "Brian"],
-    java: ["Brian"],
-    javascript: ["John", "Brian"],
-    rust: ["Michael"]
-  };
-
   it("should pass on the given solution", () => {
-    assert.equal(solutions.userBySkill(users), expected);
+    const users = [
+      {
+        id: 0,
+        name: "John",
+        skills: ["javascript", "html", "css", "c#"]
+      },
+      {
+        id: 1,
+        name: "Brian",
+        skills: ["javascript", "java", "c", "c#", "c++", "html"]
+      },
+      {
+        id: 2,
+        name: "Michael",
+        skills: ["c", "c++", "go", "rust"]
+      }
+    ];
+
+    const expected = {
+      c: ["Brian", "Michael"],
+      "c#": ["John", "Brian"],
+      "c++": ["Brian", "Michael"],
+      css: ["John"],
+      go: ["Michael"],
+      html: ["John", "Brian"],
+      java: ["Brian"],
+      javascript: ["John", "Brian"],
+      rust: ["Michael"]
+    };
+
+    assert.deepEqual(solutions.userBySkill(users), expected);
   });
 });
